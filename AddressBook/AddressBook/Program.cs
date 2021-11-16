50b59aed3aed49406cf83538a46ac4d908b590c3
Harshit <yoharshit98@gmail.com>
2021-11-17 00:36:39 +0530

    [Harshit Added: Delete User]

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -7,27 +7,41 @@ namespace AddressBook
         static void Main(string[] args)
         {
             int num;
+            string keyPress = "o";
             //guide to user 
             Console.WriteLine("__Welcome to the address book program__");
             Console.WriteLine();
             Console.WriteLine("Select the option.");
-            Console.WriteLine("1- Add contact, 2- View contact");
-            num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("1- Add contact, 2- View contact,3-edit contact,4-delete contact");
+
             Console.WriteLine();
-
-            switch (num)               //switch case 
+            AddressBook AddObj = new AddressBook();
+            //num = Convert.ToInt32(Console.ReadLine());
+            while (keyPress != "n")
             {
-                case 1:
-                    AddressBook AddObj = new AddressBook();
-                    AddObj.AddContact();
-                    AddObj.View();
-                    break;
+                Console.WriteLine("1- Add contact, 2- View contact,3-edit contact,4-delete contact");
+                num = Convert.ToInt32(Console.ReadLine());
 
-                case 2:
-                    AddressBook Addobj2 = new AddressBook();
-                    Addobj2.View();
-                    break;
+                switch (num)               //switch case 
+                {
+                    case 1:
+                        AddObj.AddContact();
+                        break;
 
+                    case 2:
+                        AddObj.View();
+                        break;
+
+                    case 3:
+                        AddObj.Edit();
+                        break;
+
+                    case 4:
+                        AddObj.Delete();                                  //method to delete the contacts
+                        break;
+                }
+                Console.WriteLine("Do you wish to continue? Press (y/n)");
+                keyPress = Console.ReadLine();
             }
         }
     }
